e7d3eabcf62a1fa0e294db8b08ada23b5dc7ebc1
Yi Wang <yiwang01@baidu.com>
2016-03-09 13:54:10 -0800

    Add more tests

--- nn_test.go
+++ nn_test.go
@@ -6,22 +6,33 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestExample(t *testing.T) {
+func TestForward(t *testing.T) {
 	deterministic = true // Deterministic initialization of parameters.
 
-	n := &Network{}
 	x := &Wire{}
 	y := &Wire{}
 
-	out := n.sig(n.dot(x, y))
+	{
+		n := &Network{}
+		out := n.sig(n.dot(x, y))
 
-	// n.out = n.sig(n.dot(
-	// 	n.sig(n.dot(n.x, n.y)),
-	// 	n.sig(n.dot(n.x, n.y))))
+		x.value = 1
+		y.value = 2
+		n.Forward()
 
-	x.value = 1
-	y.value = 2
-	n.Forward()
+		assert.Equal(t, out.value, sig(1*1+1*2+1))
+	}
+	{
+		n := &Network{}
+		out := n.sig(n.dot(
+			n.sig(n.dot(x, y)),
+			n.sig(n.dot(x, y))))
 
-	assert.Equal(t, out.value, sig(4))
+		n.Forward()
+
+		assert.Equal(t, out.value, sig(
+			sig(1*1+2*1+1)+
+				sig(1*1+2*1+1)+
+				1))
+	}
 }
