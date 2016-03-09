e98cc4b9980fa491be53ba4e1e5c4fdfce3f2562
Yi Wang <yiwang01@baidu.com>
2016-03-09 13:51:04 -0800

    Simplify test

--- nn_test.go
+++ nn_test.go
@@ -6,33 +6,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// Example is an implementation of Network.  Notice that an
-// implementation must include input and output wires.
-type Example struct {
-	*Network
-	x, y, out *Wire
-}
-
-func newExample() *Example {
-	n := &Example{
-		Network: &Network{}, //TODO(y): Call NetNetwork
-		x:       &Wire{},
-		y:       &Wire{},
-		out:     nil,
-	}
-	n.out = n.sig(n.dot(n.x, n.y))
-	return n
-}
-
-func (n *Example) run() {
-	n.x.value = 1
-	n.y.value = 2
-	n.Forward()
-}
-
 func TestExample(t *testing.T) {
-	deterministic = true
-	n := newExample()
-	n.run()
-	assert.Equal(t, n.out.value, sig(4))
+	deterministic = true // Deterministic initialization of parameters.
+
+	n := &Network{}
+	x := &Wire{}
+	y := &Wire{}
+
+	out := n.sig(n.dot(x, y))
+
+	// n.out = n.sig(n.dot(
+	// 	n.sig(n.dot(n.x, n.y)),
+	// 	n.sig(n.dot(n.x, n.y))))
+
+	x.value = 1
+	y.value = 2
+	n.Forward()
+
+	assert.Equal(t, out.value, sig(4))
 }
